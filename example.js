16553575f03c1e7f121dc2bc213e529e13d729ae
Tarun <tarun.batra00@gmail.com>
2017-07-26 18:16:42 +0530

    Adds sample code and output

---
+++ example.js
@@ -0,0 +1,6 @@
+var logem = require('./index.js');
+
+logem.debug({ objects: [ 'allowed', 'here' ] });
+logem.info('An informational message');
+logem.warn('⚠️  Warning !!');
+logem.error('ERROR: blah');
